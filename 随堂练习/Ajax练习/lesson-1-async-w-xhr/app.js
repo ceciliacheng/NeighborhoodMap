8715e12fd4c232d05ff17655bb8b4009cf2027fa
ceciliacheng <1772979678@qq.com>
2018-05-09 15:07:37 +0800

    更新练习

--- 随堂练习/Ajax练习/lesson-1-async-w-xhr/app.js
+++ 随堂练习/Ajax练习/lesson-1-async-w-xhr/app.js
@@ -10,3 +10,20 @@
         searchedForText = searchField.value;
     });
 })();
+
+const searchedForText = 'hippos';
+const unsplashRequest = new XMLHttpRequest();
+
+unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+unsplashRequest.onload = addImage;
+unsplashRequest.setRequestHeader('Authorization', 'Client-ID <26292>');
+unsplashRequest.send();
+
+function addImage(){
+}
+
+function addArticles () {}
+const articleRequest = new XMLHttpRequest();
+articleRequest.onload = addArticles;
+articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=<b764af09e34a4f7c8dbf2db628411f09>`);
+articleRequest.send();
