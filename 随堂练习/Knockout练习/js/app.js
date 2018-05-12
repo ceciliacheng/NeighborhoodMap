026f9d8f348918319cb109d0ddb5de8dde330c4a
Cecilia Cheng <1772979678@qq.com>
2018-05-12 17:15:29 +0800

    修改语法错误

--- 随堂练习/Knockout练习/js/app.js
+++ 随堂练习/Knockout练习/js/app.js
@@ -1,10 +1,12 @@
-let ViewModel = function {
+let ViewModel = function (){
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   this.imgAttribution = ko.observable("https://www.flickr.com/photos/big")
 
-  this.incrementCounter = function(){};
+  this.incrementCounter = function(){
+    this.clickCount(this.clickCount() + 1);
+  };
 }
 
 ko.applyBindings(new ViewModel());
